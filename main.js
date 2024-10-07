d4e4cc5cc5d8d98d300e3ee90f06c9d498b2fef4
Maggie Boyd <maggie.boyd@stu.ocsb.ca>
2024-10-07 11:14:47 -0400

    learnt abiout variables an constants

--- main.js
+++ main.js
@@ -13,3 +13,42 @@
 let it_is_christmas = false
 let teaching_partner = "Mr. Squirrel"
 let number_of_students = 27
+
+let length = 10
+let width = 5
+let area = length * width
+console.log("Area:", area)
+console.log("Area: " + area)
+
+console.log("\n")
+
+let first_name = "Santa"
+let last_name = "Claws"
+last_name = "Claus"
+console.log("I still believe in", first_name, last_name)
+console.log("I might believe in " + first_name + " " + last_name)
+
+console.log("\n")
+
+console.log(typeof area)
+console.log(typeof last_name)
+
+console.log("\n")
+
+const PI = 3.14
+let radius = 5
+let area_circle = PI * radius**2
+console.log("The area of the circle is:", area_circle)
+
+console.log("\n")
+
+let base = 4
+let height = 4
+area_triangle = base * height / 2
+console.log("The area of the triangle is:", area_triangle)
+
+console.log("\n")
+
+console.log(area, first_name, last_name, area_circle, area_triangle)
+
+console.log("\n")
